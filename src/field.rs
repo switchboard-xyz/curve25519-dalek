417e789c8aa31da4f46955b321b3bc251896e6ce
Lawrence Wu <larry.wu@solana.com>
2021-12-17 10:20:58 -0600

    Split function to stay under stack frame limits

--- src/field.rs
+++ src/field.rs
@@ -112,6 +112,29 @@ impl FieldElement {
 
     /// Compute (self^(2^250-1), self^11), used as a helper function
     /// within invert() and pow22523().
+    #[inline(never)]
+    fn pow22501_newframe(t2: FieldElement, t3: FieldElement) -> (FieldElement, FieldElement) {
+        let t4  = t3.square();             // 4,2,1
+        let t5  = &t2 * &t4;               // 4,3,2,1,0
+        let t6  = t5.pow2k(5);             // 9,8,7,6,5
+        let t7  = &t6 * &t5;               // 9,8,7,6,5,4,3,2,1,0
+        let t8  = t7.pow2k(10);            // 19..10
+        let t9  = &t8 * &t7;               // 19..0
+        let t10 = t9.pow2k(20);            // 39..20
+        let t11 = &t10 * &t9;              // 39..0
+        let t12 = t11.pow2k(10);           // 49..10
+        let t13 = &t12 * &t7;              // 49..0
+        let t14 = t13.pow2k(50);           // 99..50
+        let t15 = &t14 * &t13;             // 99..0
+        let t16 = t15.pow2k(100);          // 199..100
+        let t17 = &t16 * &t15;             // 199..0
+        let t18 = t17.pow2k(50);           // 249..50
+
+        (t13, t18)
+    }
+
+    /// Compute (self^(2^250-1), self^11), used as a helper function
+    /// within invert() and pow22523().
     fn pow22501(&self) -> (FieldElement, FieldElement) {
         // Instead of managing which temporary variables are used
         // for what, we define as many as we need and leave stack
@@ -128,21 +151,7 @@ impl FieldElement {
         let t1  = t0.square().square();    // 3         e_1 = 2^3
         let t2  = self * &t1;              // 3,0       e_2 = 2^3 + 2^0
         let t3  = &t0 * &t2;               // 3,1,0
-        let t4  = t3.square();             // 4,2,1
-        let t5  = &t2 * &t4;               // 4,3,2,1,0
-        let t6  = t5.pow2k(5);             // 9,8,7,6,5
-        let t7  = &t6 * &t5;               // 9,8,7,6,5,4,3,2,1,0
-        let t8  = t7.pow2k(10);            // 19..10
-        let t9  = &t8 * &t7;               // 19..0
-        let t10 = t9.pow2k(20);            // 39..20
-        let t11 = &t10 * &t9;              // 39..0
-        let t12 = t11.pow2k(10);           // 49..10
-        let t13 = &t12 * &t7;              // 49..0
-        let t14 = t13.pow2k(50);           // 99..50
-        let t15 = &t14 * &t13;             // 99..0
-        let t16 = t15.pow2k(100);          // 199..100
-        let t17 = &t16 * &t15;             // 199..0
-        let t18 = t17.pow2k(50);           // 249..50
+        let (t13, t18) = FieldElement::pow22501_newframe(t2, t3);
         let t19 = &t18 * &t13;             // 249..0
 
         (t19, t3)
