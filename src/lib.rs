a9e799a4429b24d80b49b62c7fe67b8bcf15d373
mgild <mgild@users.noreply.github.com>
2022-01-21 02:29:13 -0500

    mod

--- src/lib.rs
+++ src/lib.rs
@@ -274,7 +274,6 @@ extern crate fiat_crypto;
 // Used for traits related to constant-time code.
 extern crate subtle;
 pub use subtle;
-pub mod subtle;
 
 #[cfg(all(test, feature = "serde"))]
 extern crate bincode;
